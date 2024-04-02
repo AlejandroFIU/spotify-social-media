b65c61ea48cc85a112af507df4bc91efac1b79be
iamllcoolray <llcoolray24@gmail.com>
2024-04-02 09:51:08 -0400

    added a GET request by user ID

--- SSMApplication/src/main/java/com/spotifysocialmedia/SSMApplication/Controllers/UsersController.java
+++ SSMApplication/src/main/java/com/spotifysocialmedia/SSMApplication/Controllers/UsersController.java
@@ -3,13 +3,11 @@ package com.spotifysocialmedia.SSMApplication.Controllers;
 import com.spotifysocialmedia.SSMApplication.ConnectionFactory.DBConnection;
 import com.spotifysocialmedia.SSMApplication.Models.Users;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 
@@ -18,16 +16,19 @@ public class UsersController {
     private DBConnection dbConnection;
     private Connection connection;
     private Statement statement;
+    private PreparedStatement preparedStatement;
     private ResultSet resultSet;
     private Users user;
 
-    public UsersController(){
-        dbConnection = new DBConnection();
-        connection = dbConnection.connectionFactory();
-    }
+//    public UsersController(){
+//        dbConnection = new DBConnection();
+//        connection = dbConnection.connectionFactory();
+//    }
 
     @GetMapping("/users/all")
     public @ResponseBody ArrayList<Users> getAllUsers() throws SQLException {
+        dbConnection = new DBConnection();
+        connection = dbConnection.connectionFactory();
         ArrayList<Users> users = new ArrayList<Users>();
         int userID, silence, mod;
         String name, email, username, password;
@@ -65,5 +66,48 @@ public class UsersController {
         }
 
         return users;
+    }
+
+    @GetMapping("/users/{id}")
+    public @ResponseBody Users getUserById(@PathVariable("id") int id) throws SQLException {
+        dbConnection = new DBConnection();
+        connection = dbConnection.connectionFactory();
+        int userID, silence, mod;
+        String name, email, username, password;
+        boolean isSilenced = false, isMod = false;
+        try {
+            preparedStatement = connection.prepareStatement("select * from users where userID = ?;");
+            preparedStatement.clearParameters();
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                userID = resultSet.getInt("userID");
+                name = resultSet.getString("name");
+                email = resultSet.getString("email");
+                username = resultSet.getString("username");
+                password = resultSet.getString("password");
+                silence = resultSet.getInt("isSilenced");
+                mod = resultSet.getInt("isMod");
+                if (silence == 1)
+                    isSilenced = true;
+                if (mod == 1)
+                    isMod = true;
+                user = new Users(userID, name, email, username, password, isSilenced, isMod);
+            }
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (resultSet != null)
+                resultSet.close();
+            if (preparedStatement != null)
+                preparedStatement.close();
+            if (connection != null)
+                connection.close();
+        }
+
+        return user;
     }
 }
